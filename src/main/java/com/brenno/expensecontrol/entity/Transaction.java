8ce08910c1f2e3c3345e51053738fca0760768b4
BrennoPaiva2310 <brennopaivag@gmail.com>
2026-04-13 12:12:48 -0300

    feat: add entities and attributes

---
+++ src/main/java/com/brenno/expensecontrol/entity/Transaction.java
@@ -0,0 +1,37 @@
+package com.brenno.expensecontrol.entity;
+
+import enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
+}
